9520bfe69469cb2a74d8f030ef9829d204ef937c
pudueti naveen <pudupetinaveen18@gmail.com>
2024-06-07 18:14:46 +0530

    Add files via upload

---
+++ DataEncapsulations.java
@@ -0,0 +1,18 @@
+class DataEncapsulations{
+	double balanceAmount=12560.34;
+	String userName;
+	private Stringg password;
+	//it won't participates in profiling process
+	private String aadharCardId;
+	//It won't participates in profiling process
+	String pancard;
+	
+public static void main(String[] args){
+
+
+
+}
+}
+
+//to see the profile of class
+//Syntax::javap className
